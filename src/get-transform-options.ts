7fede71e8280b3ee830b81aa08c97abedc7df2dd
Timothée “Tim” Pillard <github@tpillard.fr>
2024-01-23 01:05:49 +0100

    0.3.0 changes
    - add TypeScript@5.3.3 with TS Project References
    - export types
    - update Vite
    - match Vite Node.js runtime requirements
    - add Vitest & write some tests
    - make the code a bit more modular & easier to test
    - add some quality check scripts: typecheck, check (format & lint)

---
+++ src/get-transform-options.ts
@@ -0,0 +1,34 @@
+import type { JscConfig } from "@swc/core";
+import type { Params } from "./types.js";
+
+export function getTransformOptions({
+	swcOptions,
+	jsc,
+	legacyDecorator,
+}: {
+	swcOptions: NonNullable<Params["swcOptions"]>;
+	jsc: JscConfig | null;
+	legacyDecorator: boolean;
+}) {
+	return {
+		swcrc: false,
+		configFile: false,
+		inputSourceMap: false,
+		sourceMaps: true,
+		...swcOptions,
+		...(jsc && {
+			jsc: legacyDecorator
+				? {
+						...jsc,
+						keepClassNames: true,
+						parser: {
+							decorators: true,
+							decoratorsBeforeExport: true,
+							syntax: "typescript",
+							...jsc.parser,
+						},
+				  }
+				: jsc,
+		}),
+	};
+}
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import {
+	type FilterPattern as RollupFilterPattern,
+	createFilter,
+} from "@rollup/pluginutils";
+import { type Options as SWCOptions, transform } from "@swc/core";
+import type { Plugin } from "vite";
+
+import { checkSwcOptionsOnce } from "./check-options-once.js";
+import { getTransformOptions } from "./get-transform-options.js";
+import type { Params } from "./types.js";
+
+const EMPTY_OBJECT = Object.freeze({});
+
+let _swcOptions = EMPTY_OBJECT;
+
+function transformFile(
+	id: string,
+	code: string,
+	swcOptions: NonNullable<Params["swcOptions"]>,
+	suppressLegacyDecoratorNoExplicitUDFCFWarning: NonNullable<
+		Params["suppressLegacyDecoratorNoExplicitUDFCFWarning"]
+	>,
+) {
+	const [filepath] = id.split("?", 1);
+
+	if (_swcOptions === EMPTY_OBJECT) {
+		const { jsc, legacyDecorator } = checkSwcOptionsOnce({
+			swcOptions,
+			suppressLegacyDecoratorNoExplicitUDFCFWarning,
+		});
+		_swcOptions = getTransformOptions({ swcOptions, jsc, legacyDecorator });
+	}
+
+	const transformOptions: SWCOptions = Object.assign(
+		{
+			filename: id,
+			sourceFileName: filepath,
+		},
+		_swcOptions,
+	);
+
+	return transform(code, transformOptions);
+}
+
+export default function createViteSWCTransformPlugin({
+	include,
+	exclude,
+	suppressLegacyDecoratorNoExplicitUDFCFWarning = false,
+	swcOptions,
+}: Params = {}) {
+	const filter = createFilter(include ?? /\.tsx?$/, exclude ?? /node_modules/);
+	return {
+		name: "swc-transform",
+		enforce: "pre",
+		config() {
+			return {
+				esbuild: false,
+			};
+		},
+		configResolved(resolvedConfig) {},
+		transform(code, id) {
+			if (!filter(id)) return null;
+			return transformFile(
+				id,
+				code,
+				swcOptions ?? EMPTY_OBJECT,
+				suppressLegacyDecoratorNoExplicitUDFCFWarning,
+			);
+		},
+	} satisfies Plugin;
+}
+
+export type { Params, RollupFilterPattern, SWCOptions };
